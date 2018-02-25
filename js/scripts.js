ef86875cb11d56647c4d240aafb7ffd210992d6a
Megan Lee <melee124@gmail.com>
2018-02-25 10:24:12 -0800

    added else if statements for the different vacation outcomes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
-var location = $("#location").val();
-var temperature = $("input:radio[name=temperature]:checked").val();
-var distance = $("select#distance").val();
-var swim = $("select#swim").val();
-var shoesInput = $("input#shoes").val();
+  $("form#userheight").submit(function(event) {
+  var location = $("location").val();
+  var temperature = $("input:radio[name=temperature]:checked").val());
+  var distance = $("distance").val());
+  var swim = $("swim").val();
+  var shoes = $("input#shoes").val();
 
-  if (age > 21) {
-    $('#drinks').show();
-  } else if (age === 21) {
-    alert("Now don't go crazy!");
-    $('#drinks').show();
-  } else {
-    $('#under-21').show();
+  if (swim === 'no') {
+    $('#no-swim').show();
+  } else if (temperature === 'hot' && shoes === 'sandals') {
+    $('#tahiti').show();
+  } else if (shoes === 'snowboots' && location === 'desert'){
+    $('antarctica').show();
+    else if (shoes === 'heels' && location === 'city' && )
+    $('#short-height').show();
   }
+    event.preventDefault();
+  });
 });
