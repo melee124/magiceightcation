4a3f256fd295e8be78327a4213d9d2152043a763
Megan Lee <melee124@gmail.com>
2018-02-25 21:03:26 -0800

    Added a name function and more button styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
-  $("form#travel").submit(function(event) {  
+  $("form#travel").submit(function(event) {
+    onSubmit('#travel').hide;
   var temperature = parseInt($("input#temperature").val());
   var distance = parseInt($("input#distance").val());
   var swim = parseInt($("#swim").val());
   var location = parseInt($("#location").val());
   var shoes = parseInt($("#shoes").val());
+  var usernameInput = $("input#username").val();
+
+  $(".username").text(usernameInput);
+
   if (temperature < 50 && distance < 100) {
     $('#cold-weather').show();
   } else if (swim > 3 && location === 2) {
